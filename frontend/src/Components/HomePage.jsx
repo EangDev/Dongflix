8449b70796e647b95221e2fb185e541f3d07606d
EangDev <eangdev.gamedev@gmail.com>
2025-10-28 13:29:55 +0700

    create-header-of-homepage

--- frontend/src/Components/HomePage.jsx
+++ frontend/src/Components/HomePage.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment, faUser, faMagnifyingGlass, faHouse,   
+faComments, faBook, faEnvelope
+} from "@fortawesome/free-solid-svg-icons";
 import './style/HomePageStyle.css';
-import logo from '../Assets/loading.gif';
+import logo from '../Assets/mylogo.png';
 
 function HomePage() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -12,13 +16,47 @@ function HomePage() {
   };
 
   return(
-    <div className="Header">
-        <header>
-            <div className="logoImg">
-                <img src={logo} alt="" />
-            </div>
-        </header>
-    </div>
+    <header>
+      <div className="header-container">
+        <div className="logo-img">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="search-box">
+          <input type="text" className="search" placeholder="Search..."/>
+          <button id="btn-search">
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="Magnifying" color="white" size="3px"/>
+          </button>
+        </div>
+        <div className="menu-text">
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faHouse} color="white" size="2px"/>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faUser} color="white" size="2px"/>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faComment} color="white" size="2px"/>
+              <a href="#">Contact Us</a>
+            </li>
+            <li className="fixing-policy">
+              <FontAwesomeIcon icon={faBook} color="white" size="2px"/>
+              <a href="#">Privacy Policy</a>
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faComments} color="white" size="2px"/>
+              <a href="#">FQA</a>
+            </li>
+            <li className="fixing-request">
+              <FontAwesomeIcon icon={faEnvelope} color="white" size="2px"/>
+              <a href="#">Request Drama</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
   );
 }
 
